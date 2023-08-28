77801963117151e11884e23cce30703987e174de
Kevin Ireri <chosenireri42@gmail.com>
2023-08-28 23:53:38 +0300

    charts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Stats from '../components/Stats'
+import TransactionChart from '../components/TranscationChart'
+import ProfilePieChart from '../components/ProfilePieChart'
 
 
 export default function Dashboard() {
 	return (
 		<div className="flex flex-col gap-4">
 			<Stats />
+      <div className="flex flex-row gap-4 w-full">
+				<TransactionChart />
+				<ProfilePieChart />
+			</div>
 		</div>
 	)
 }
